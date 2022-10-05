e0165f26a082b00ae24246584faa422e2b11f872
Julian Knight <1591850+TotallyInformation@users.noreply.github.com>
2022-10-05 18:05:10 +0100

    Dummy msg

--- templates/master/bin/restartnr.js
+++ templates/master/bin/restartnr.js
@@ -2,5 +2,6 @@
 'use strict'
 // @ts-nocheck
 
+console.log('Sorry, nothing here at the moment, one-day this might try to restart node-red in multiple ways depending on how it is installed.')
 
 //EOF
